6e21f66bdf863bce0bf09ec7c22054b3b12bb885
Kobby Pentangeli <kobbypentangeli@gmail.com>
2021-04-04 08:49:11 +0000

    refactor: Edit Message.rs

--- src/message.rs
+++ src/message.rs
@@ -3,26 +3,12 @@ use std::net::SocketAddr;
 
 #[derive(Serialize, Deserialize)]
 pub enum Message {
+    /// Peer's public address
     MyPubAddr(SocketAddr),
-
-    // Request for list of peers
+    /// Request for list of peers
     GiveMeAListOfPeers,
-
-    // Response for GiveMeAListOfPeers with peers info
+    /// Response for GiveMeAListOfPeers with peers info
     TakePeersList(Vec<SocketAddr>),
-
-    // Some random message
+    /// Some random message
     Info(String),
 }
-
-// impl Message {
-//     // serialization by bincode
-
-//     pub fn ser(&self) -> Result<Vec<u8>> {
-//         bincode::serialize(self)
-//     }
-
-//     pub fn de(data: &Vec<u8>) -> Result<Self> {
-//         bincode::deserialize(data)
-//     }
-// }
